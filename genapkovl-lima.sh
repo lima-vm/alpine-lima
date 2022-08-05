d2e992cb65a179e8e45da749dc953d6b6dbc4aa1
Mark Yen <mark.yen@suse.com>
2022-08-04 17:26:14 -0700

    genapkovl-lima: make /etc/conf.d
    Need to create the directory before writing to it.
    
    Signed-off-by: Mark Yen <mark.yen@suse.com>

--- genapkovl-lima.sh
+++ genapkovl-lima.sh
@@ -196,6 +196,7 @@ if [ "${LIMA_INSTALL_BINFMT_MISC}" == "true" ]; then
     sed -i "/^FMTS=/a \\\t${magic} ${mask} ${arch}" "${tmp}/etc/init.d/qemu-binfmt"
 
     # qemu from tonistiigi/binfmt is patched to assume preserve-argv; set it here.
+    mkdir -p "${tmp}/etc/conf.d"
     echo 'binfmt_flags="POCF"' > "${tmp}/etc/conf.d/qemu-binfmt"
 
     rc_add qemu-binfmt default
